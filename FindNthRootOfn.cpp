baadfb72fbb6eeed69ed7a0c2001be07bf3086eb
vivek Singh <vivekkumar52522@gmail.com>
2023-04-20 15:17:32 +0530

    find Nth Root of N

---
+++ FindNthRootOfn.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+	int NthRoot(int n, int m)
+	{
+	    int low=0;
+	    int high=m;
+	    while(low<=high){
+	        int mid=low+(high-low)/2;
+	        if(pow(mid,n)==m){
+	            return mid;
+	        }
+	        else if(pow(mid,n)>m){
+	            high=mid-1;
+	        }else{
+	            low=mid+1;
+	        }
+	    }
+	    return -1;
+	}  
